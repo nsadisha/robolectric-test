378f7231cfbb92909ccd13a7aac4d8f96e30551d
nsadisha <nsadisha@gmail.com>
2022-05-09 23:55:10 +0530

    fragment test case

--- app/src/test/java/com/nsadisha/android/roboelectrictest/RobolectricTest.kt
+++ app/src/test/java/com/nsadisha/android/roboelectrictest/RobolectricTest.kt
@@ -1,7 +1,10 @@
 package com.nsadisha.android.roboelectrictest
 
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.widget.TextView
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
@@ -10,12 +13,36 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class RobolectricTest {
 
+    private var activity: MainActivity? = null
+    private var fragment: FirstFragment? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        fragment = FirstFragment()
+        activity = Robolectric.buildActivity(
+            MainActivity::class.java
+        )
+            .create()
+            .start()
+            .resume()
+            .get()
+
+        val fragmentManager: FragmentManager = activity!!.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(fragment!!, null)
+        fragmentTransaction.commit()
+    }
+
     @Test
-    fun is_text_view_available_with_hello_world(){
-        val mainActivity: MainActivity = Robolectric.setupActivity(MainActivity::class.java)
+    fun is_text_view_available_in_Main_activity(){
+        val textView: TextView = activity!!.findViewById(R.id.activityText)
+        Assert.assertEquals("Hello World from MainActivity", textView.text.toString())
+    }
 
-        val textView: TextView = mainActivity.findViewById(R.id.textView)
-
-        Assert.assertEquals("Hello World", textView.text.toString())
+    @Test
+    fun is_text_view_available_in_fragment(){
+        val textView: TextView = activity!!.findViewById(R.id.fragmentText)
+        Assert.assertEquals("Hello World from Fragment", textView.text.toString())
     }
 }
